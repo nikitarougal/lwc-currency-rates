4b0639847f44eba58e48e4e05fe561d8cf2982e1
Your Name <surinamio@gmail.com>
2023-07-10 11:00:19 -0400

    add ExchangeRateCallout class and connected currencyTab with the class

--- force-app/main/default/lwc/currencyTable/currencyTable.js
+++ force-app/main/default/lwc/currencyTable/currencyTable.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from "lwc";
 import { subscribe, MessageContext } from "lightning/messageService";
 import CURRENCY_UPDATED_CHANNEL from "@salesforce/messageChannel/Currency_Updated__c";
+import getExchangeRates from "@salesforce/apex/ExchangeRateCallout.getExchangeRates";
 
 const COLS = [
   { label: "Currency", fieldName: "Currency" },
@@ -8,14 +9,14 @@ const COLS = [
   { label: "Rate", fieldName: "Rate" }
 ];
 
-const rates = [
-  { Id: "1", Currency: "USD", CurrencyName: "Dollar", Rate: 1 },
-  { Id: "2", Currency: "EUR", CurrencyName: "Euro", Rate: 0.9 },
-  { Id: "3", Currency: "CAD", CurrencyName: "Canadian Dollar", Rate: 1.3 }
-];
+// const rates = [
+//   { Id: "1", Currency: "USD", CurrencyName: "Dollar", Rate: 1 },
+//   { Id: "2", Currency: "EUR", CurrencyName: "Euro", Rate: 0.9 },
+//   { Id: "3", Currency: "CAD", CurrencyName: "Canadian Dollar", Rate: 1.3 }
+// ];
 export default class DatatableCustomDataType extends LightningElement {
   columns = COLS;
-  rates = rates;
+  rates = [];
   @wire(MessageContext)
   messageContext;
   subscribeToMessageChannel() {
@@ -25,11 +26,16 @@ export default class DatatableCustomDataType extends LightningElement {
       (message) => this.handleMessage(message)
     );
   }
-  handleMessage(message) {
+
+  async handleMessage(message) {
     const selectedCurrency = message.selectedCurrency;
+
+    if (this.rates.length === 0) {
+      await this.getExchangeRateData();
+    }
     const prevBaseRate = this.getRateByCurrency(selectedCurrency);
     if (prevBaseRate !== 1) {
-      rates.forEach((rate) => {
+      this.rates.forEach((rate) => {
         if (rate.Currency === selectedCurrency) {
           rate.Rate = 1;
         } else {
@@ -38,7 +44,7 @@ export default class DatatableCustomDataType extends LightningElement {
         }
       });
 
-      this.rates = [...rates];
+      this.rates = [...this.rates];
       console.log(this.rates);
     }
   }
@@ -47,8 +53,30 @@ export default class DatatableCustomDataType extends LightningElement {
     this.subscribeToMessageChannel();
   }
 
+  getExchangeRateData() {
+    getExchangeRates()
+      .then((result) => {
+        console.log(result);
+        this.rates = [
+          {
+            Currency: result.base,
+            CurrencyName: "Dollar",
+            Rate: 1
+          },
+          ...Object.entries(result.rates).map(([currency, rate]) => ({
+            Currency: currency,
+            CurrencyName: currency === "EUR" ? "EURO" : "Canadian Dollar",
+            Rate: parseFloat(rate).toFixed(2)
+          }))
+        ];
+      })
+      .catch((error) => {
+        console.error("Error retrieving exchange rate data:", error);
+      });
+  }
+
   getRateByCurrency(currency) {
-    const rateObj = rates.find((rate) => rate.Currency === currency);
+    const rateObj = this.rates.find((rate) => rate.Currency === currency);
     return rateObj ? rateObj.Rate : null;
   }
 }
